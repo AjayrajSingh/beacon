bd8086698a1dac43a0ad58b7281caa2e090f6df8
AjayrajSingh <shiningcoders@gmail.com>
2021-04-07 19:17:23 +0530

    Added google fonts in the app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:beacon/Screens/Splash/splash.dart';
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(Beacon());
@@ -12,6 +13,11 @@ class Beacon extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.ralewayTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       title: 'Beacon',
       home: Splash(),
     );
