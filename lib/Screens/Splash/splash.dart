9db07d17867b2f3b23f8eaa021549a7954b49195
AjayrajSingh <shiningcoders@gmail.com>
2021-04-08 23:18:56 +0530

    Added login based routing

--- lib/Screens/Splash/splash.dart
+++ lib/Screens/Splash/splash.dart
@@ -1,13 +1,27 @@
 import 'package:beacon/Screens/Onboarding/onboarding.dart';
+import 'package:beacon/Screens/homeScreen.dart';
+import 'package:beacon/Services/authServices.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flare_splash_screen/flare_splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class Splash extends StatelessWidget {
+  AuthServices _authServices = AuthServices();
+
   @override
   Widget build(BuildContext context) {
     return SplashScreen(
       'assets/beaconSplash.flr',
-      Onboarding(),
+      FutureBuilder(
+        future: _authServices.getCurrentUser(),
+        builder: (context, AsyncSnapshot<User> snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return Onboarding();
+          }
+        },
+      ),
       backgroundColor: Colors.white,
       startAnimation: 'Intro',
     );
