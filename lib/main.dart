1be00878f49dfbe0640ac940cc5a8b2816841268
AjayrajSingh <shiningcoders@gmail.com>
2021-04-08 23:22:22 +0530

    Initialized firebase app

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,24 @@
 import 'package:beacon/Screens/Splash/splash.dart';
-import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'package:beacon/Theme/beaconColors.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(Beacon());
 }
 
 class Beacon extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the root of yotheur application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        primaryColor: BeaconColors.primary,
+        accentColor: BeaconColors.secondary,
         textTheme: GoogleFonts.ralewayTextTheme(
           Theme.of(context).textTheme,
         ),
